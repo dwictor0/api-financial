2f8fc205087a909c924b02151bf56283614485ef
dwictor0 <daniel.wdev0@gmail.com>
2026-05-23 15:10:59 -0300

    FEAT: criando model webhook_event definindo colunas e validacoes

--- models/webhook_event.go
+++ models/webhook_event.go
@@ -1,1 +1,14 @@
 package models
+
+import (
+	"time"
+)
+
+type WebhookEvent struct {
+	ID           uint      `gorm:"primaryKey" json:"id"`
+	EventID      string    `gorm:"uniqueIndex;not null" json:"event_id"`
+	CardID       string    `gorm:"not null" json:"card_id"`
+	ClienteEmail string    `gorm:"not null" json:"cliente_email"`
+	Timestamp    time.Time `json:"timestamp"`
+	CreatedAt    time.Time `json:"created_at"`
+}
