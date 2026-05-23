204a6e92f823e0e0a6c99e5cc40fa91d90444697
dwictor0 <daniel.wdev0@gmail.com>
2026-05-23 15:06:25 -0300

    FEAT: instalando postgresql e implementando conexao ao banco de dados via credenciais

--- config/database.go
+++ config/database.go
@@ -1,1 +1,46 @@
 package config
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func ConnectDB() *gorm.DB {
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("Erro ao inicializar as configuracoes de ambiente.")
+	}
+
+	host := getEnv("DB_HOST", "localhost")
+	user := getEnv("DB_USER", "postgres")
+	password := getEnv("DB_PASSWORD", "root")
+	dbName := getEnv("DB_NAME", "api_financial")
+	port := getEnv("DB_PORT", "5432")
+
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=America/Sao_Paulo",
+		host, user, password, dbName, port)
+
+	database, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Falha ao conectar ao banco de dado: ", err)
+	}
+
+	fmt.Printf("Banco de dados inicializado [%s!\n", dbName)
+
+	DB = database
+	return DB
+}
+
+func getEnv(key, fallback string) string {
+	if value, exists := os.LookupEnv(key); exists && value != "" {
+		return value
+	}
+	return fallback
+}
