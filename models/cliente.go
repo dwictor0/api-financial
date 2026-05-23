da2123861a41997e8b05843da0f181e93cbfa78f
dwictor0 <daniel.wdev0@gmail.com>
2026-05-23 16:08:03 -0300

    FIX: removendo caracteres especiais struct model client

--- models/cliente.go
+++ models/cliente.go
@@ -12,8 +12,8 @@ type Cliente struct {
 	ClienteEmail    string         `gorm:"uniqueIndex;not null" json:"cliente_email" binding:"required,email"`
 	TipoSolicitacao string         `gorm:"not null" json:"tipo_solicitacao" binding:"required"`
 	ValorPatrimonio float64        `gorm:"not null" json:"valor_patrimonio" binding:"required"`
-	Status          string         `gorm:"default:'Aguardando Análise'" json:"status"`
-	Prioridade      string         `gorm:"default:'não definida'" json:"prioridade"`
+	Status          string         `gorm:"default:'aguardando_analise'" json:"status"`
+	Prioridade      string         `gorm:"default:'nao_definida'" json:"prioridade"`
 	CreatedAt       time.Time      `json:"created_at"`
 	UpdatedAt       time.Time      `json:"updated_at"`
 	DeletedAt       gorm.DeletedAt `gorm:"index" json:"-"`
