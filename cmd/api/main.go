5f281b6f80828a71ef58967894ef1f9348c3c789
dwictor0 <daniel.wdev0@gmail.com>
2026-05-23 15:18:57 -0300

    FEAT: instalando gin e instanciando metodo de conexao ao banco de dados na funcao main

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,5 +1,24 @@
 package main
 
-func main() {
+import (
+	"api-financial/config"
+	"log"
 
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	config.ConnectDB()
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+			"status":  "running",
+		})
+	})
+	log.Println("Servidor HTTP iniciado na porta :8080")
+
+	if err := r.Run("8080"); err != nil {
+		log.Fatalf("Falha ao iniciar o servidor: %v", err)
+	}
 }
