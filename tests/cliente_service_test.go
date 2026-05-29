e324cf415a6bf703e17439268b78d5af9be7466c
dwictor0 <daniel.wdev0@gmail.com>
2026-05-29 20:40:51 -0300

    FIX: deletando funcao test main cliente service

--- tests/cliente_service_test.go
+++ tests/cliente_service_test.go
@@ -38,20 +38,6 @@ func SetupTestDB() *gorm.DB {
 	return globalDB
 }
 
-func TestMain(m *testing.M) {
-	os.Setenv("PIPEFY_TOKEN", "token_fake_teste_ci")
-	os.Setenv("PIPEFY_PIPE_ID", "999999")
-	os.Setenv("PIPEFY_API_URL", "https://api.pipefy.com")
-
-	code := m.Run()
-
-	os.Unsetenv("PIPEFY_TOKEN")
-	os.Unsetenv("PIPEFY_PIPE_ID")
-	os.Unsetenv("PIPEFY_API_URL")
-
-	os.Exit(code)
-}
-
 func TestCriarCliente_Sucesso(t *testing.T) {
 	db := SetupTestDB()
 	testLogger := slog.New(slog.NewTextHandler(io.Discard, nil))
